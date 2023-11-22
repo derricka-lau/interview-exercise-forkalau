6bb3e4c13fa9295680d3dd00c2b970cc883bbe3c
ForKa Lau <lauf@cardiff.ac.uk>
2023-11-22 02:18:18 +0000

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,135 @@
-import Image from 'next/image'
+"use client"
+
+import { openDayData } from '@/data/OpenDay'
+import { useMemo, useState } from 'react'
 
 export default function Home() {
+  const [selectedTab, setSelectedTab] = useState<number | null>(null)
+  const [sortColumn, setSortColumn] = useState<string | null>(null)
+  const [sortDirection, setSortDirection] = useState<string>('asc')
+  const [searchTerm, setSearchTerm] = useState<string>('')
+
+  const handleTabClick = (topicId: number) => {
+    setSelectedTab((prevTab) => (prevTab === topicId ? null : topicId))
+  }
+
+  const handleSort = (column: string) => {
+    if (sortColumn === column) {
+      setSortDirection((prevDir) => (prevDir === 'asc' ? 'desc' : 'asc'))
+    } else {
+      setSortColumn(column)
+      setSortDirection('asc')
+    }
+  }
+
+  const parseDateString = (dateString: string) => {
+    const [datePart, timePart] = dateString.split(' ')
+    const [year, month, day] = datePart.split('-').map(Number)
+    const [hour, minute, second] = timePart.split(':').map(Number)
+    return new Date(year, month - 1, day, hour, minute, second)
+  }
+
+  const filteredPrograms = useMemo(() => {
+    if (selectedTab) {
+      return openDayData.topics
+      .find((topic) => topic.id === selectedTab)
+      ?.programs.filter((program) =>
+        program.title.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    }
+  }
+  , [searchTerm, selectedTab])
+
+  const sortedPrograms = useMemo(() => {
+    if (filteredPrograms && sortColumn) {
+      return filteredPrograms.sort((a, b) => {
+        let valueA = a[sortColumn as keyof typeof a]
+        let valueB = b[sortColumn as keyof typeof b]
+        if (typeof valueA === 'string' && typeof valueB === 'string') {
+          if (sortColumn === 'start_time' || sortColumn === 'end_time') {
+            const parsedDate1 = parseDateString(valueA)
+            const parsedDate2 = parseDateString(valueB)
+            if (sortDirection === 'asc') {
+              return parsedDate1 > parsedDate2 ? -1 : 1
+            } else {
+              return parsedDate1 < parsedDate2 ? -1 : 1
+            }
+          }
+          
+          if (sortColumn === 'location') {
+            valueA = a.location.title
+            valueB = b.location.title
+          }
+
+          if (sortDirection === 'asc') {
+            return valueA.localeCompare(valueB)
+          } else {
+            return valueB.localeCompare(valueA)
+          } 
+        }
+        return 1
+      })
+    } else {
+      return filteredPrograms
+    }
+  }, [filteredPrograms, sortColumn, sortDirection])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="container mx-auto py-8">
+      <h1 className="flex justify-center text-2xl mb-4">Topics</h1>
+      <div className="flex flex-wrap">
+        {openDayData.topics.map((topic) => (
+          <div
+            key={topic.id}
+            onClick={() => handleTabClick(topic.id)}
+            className={`cursor-pointer px-2 py-1 border border-gray-300 rounded-t-lg mb-2 mr-2 ${selectedTab === topic.id ? 'bg-blue-500 text-white' : ''
+              }`}
+            style={{ maxWidth: '150px' }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            {topic.name}
+          </div>
+        ))}
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="mt-4">
+        <input
+          type="text"
+          placeholder="Search by programme title"
+          className="w-1/2 border border-gray-300 p-2 rounded"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
+        <table className="mt-4 w-full table-auto">
+          <thead>
+            <tr>
+              <th
+                onClick={() => handleSort('title')}
+                className="cursor-pointer"
+              >
+                Program Title
+              </th>
+              <th
+                onClick={() => handleSort('start_time')}
+                className="cursor-pointer">Start Time</th>
+              <th
+                onClick={() => handleSort('end_time')}
+                className="cursor-pointer">End Time</th>
+              <th onClick={() => handleSort('location')}
+                className="cursor-pointer">Location</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedPrograms?.map((program) => (
+              <tr key={program.id}>
+                <td>{program.title}</td>
+                <td>{program.start_time}</td>
+                <td>{program.end_time}</td>
+                <td>{program.location.title}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
